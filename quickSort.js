009de336780953b2fdd5a1f17cd3a7aadfe90902
gorsargs <gorsargsyanm@gmail.com>
2021-07-19 05:43:12 +0400

    Added quick sort

---
+++ quickSort.js
@@ -0,0 +1,40 @@
+function quickSort(start, end, arr) {
+    if (start < end) {
+        let pIndex = partition(start, end, arr);
+        quickSort(start, pIndex - 1, arr);
+        quickSort(pIndex + 1, end, arr);
+    };
+
+};
+
+function partition(start, end, arr) {
+    let pivot = arr[end];
+    let i = start - 1;
+    for (let j = start; j < end; j++) {
+
+        if (arr[j] < pivot) {
+            i++
+            swap(arr, i, j)
+
+        };
+    };
+    swap(arr, i + 1, end)
+    return i + 1;
+};
+
+
+
+
+
+function swap(arr, left, right) {
+    let temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+};
+
+
+const arr = [-10,10,10,100,-34,1,2,3]
+
+quickSort(0,arr.length-1,arr);
+
+console.log(arr);
